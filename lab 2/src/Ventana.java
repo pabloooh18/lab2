7710ca111d1920a4ff6e8081fe71b7130892d6bc
Alumno <Alumno@EQ29-LAB1>
2014-04-09 19:38:55 -0300

    agregar ventana

---
+++ lab 2/src/Ventana.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class Ventana {
+	
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		frame.setTitle("ventanaa!");
+		frame.setSize(800,600);
+		frame.setVisible(true);
+		
+	}
+
+
+}
